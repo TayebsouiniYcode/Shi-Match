8442f15909d2c21427cd3c242fdf41b278c9d423
TayebsouiniYcode <TayebsouiniYcode@gmail.com>
2022-06-09 15:59:55 +0100

    View-match-v1
    Création de la page matchs avec 2 type de match full-time ou live
    les match avec une date même type des matchs full-time

--- views/matchs.php
+++ views/matchs.php
@@ -1,4 +1,222 @@
-<?php 
-echo "this is a matchs view";
+<div class="primary-content  col-lg-9 col-12 ">
+    <div class="search-bar mt-5 p-3 bg-light rounded ">
+        <form action="">
+            <div class="fields d-flex gap-3 justify-content-between">
+                <div class="form-group w-50">
+                    <label class="form-label">Start date</label>
+                    <input class="form-control" type="date" name="start_date">
+                </div>
+                <div class="form-group w-50">
+                    <label class="form-label">End date</label>
+                    <input class="form-control" type="date" name="end_date">
+                </div>
+            </div>
+            <div class="fields d-flex gap-3 justify-content-between">
+                <div class="form-group w-50">
+                    <label class="form-label">Team name</label>
+                    <input class="form-control" type="text" name="team_name" placeholder="Team name">
+                </div>
+                <div class="form-group w-50">
+                    <label class="form-label">Player name</label>
+                    <input class="form-control" type="text" name="player_name" placeholder="Player name">
+                </div>
+            </div>
+            <div class="buttons d-flex justify-content-end">
+                <input type="submit" class="btn btn-primary mt-4" value="find">
+            </div>
+        </form>
+    </div>
 
-?>
+    <div class="matchs-list my-5 rounded">
+        <h3>Search result</h3>
+        <hr>
+        <div class="matchs d-flex flex-column gap-3">
+            <div class="match d-flex flex-column align-items-center">
+                <div class="time bg-success text-light text-center w-25">
+                    <span>live</span>
+                </div>
+
+                <div class="match-details match-live d-flex justify-content-around w-100 rounded-3">
+                    <div class="team-one d-flex gap-5 align-items-center">
+                        <div class="team-logo">
+                            <img src="/assets/img/team-logo2.png" alt="" width="50">
+                        </div>
+                        <div class="team-name">
+                            <span>Namek</span>
+                        </div>
+                    </div>
+                    <div class="result d-flex gap-5 align-items-center">
+                        <span>4</span>
+                        <span>-</span>
+                        <span>1</span>
+                    </div>
+                    <div class="team-two d-flex gap-5 align-items-center">
+                        <div class="team-name">
+                            <span>Tallakht</span>
+                        </div>
+                        <div class="team-logo">
+                            <img src="/assets/img/team-logo.png" alt="" width="50">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="match d-flex flex-column align-items-center">
+                <div class="time bg-success text-light text-center w-25">
+                    <span>live</span>
+                </div>
+
+                <div class="match-details match-live d-flex justify-content-around w-100 rounded-3">
+                    <div class="team-one d-flex gap-5 align-items-center">
+                        <div class="team-logo">
+                            <img src="/assets/img/team-logo2.png" alt="" width="50">
+                        </div>
+                        <div class="team-name">
+                            <span>Namek</span>
+                        </div>
+                    </div>
+                    <div class="result d-flex gap-5 align-items-center">
+                        <span>4</span>
+                        <span>-</span>
+                        <span>1</span>
+                    </div>
+                    <div class="team-two d-flex gap-5 align-items-center">
+                        <div class="team-name">
+                            <span>Tallakht</span>
+                        </div>
+                        <div class="team-logo">
+                            <img src="/assets/img/team-logo.png" alt="" width="50">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="match d-flex flex-column align-items-center">
+                <div class="time bg-success text-light text-center w-25">
+                    <span>live</span>
+                </div>
+
+                <div class="match-details match-live d-flex justify-content-around w-100 rounded-3">
+                    <div class="team-one d-flex gap-5 align-items-center">
+                        <div class="team-logo">
+                            <img src="/assets/img/team-logo2.png" alt="" width="50">
+                        </div>
+                        <div class="team-name">
+                            <span>Namek</span>
+                        </div>
+                    </div>
+                    <div class="result d-flex gap-5 align-items-center">
+                        <span>4</span>
+                        <span>-</span>
+                        <span>1</span>
+                    </div>
+                    <div class="team-two d-flex gap-5 align-items-center">
+                        <div class="team-name">
+                            <span>Tallakht</span>
+                        </div>
+                        <div class="team-logo">
+                            <img src="/assets/img/team-logo.png" alt="" width="50">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="match d-flex flex-column align-items-center">
+                <div class="time bg-secondary text-light text-center w-25">
+                    <span>Full time</span>
+                </div>
+                <div class="match-details match-full-time d-flex justify-content-around w-100 rounded-3">
+                    <div class="team-one d-flex gap-5 align-items-center">
+                        <div class="team-logo">
+                            <img src="/assets/img/team-logo2.png" alt="" width="50">
+                        </div>
+                        <div class="team-name">
+                            <span>Namek</span>
+                        </div>
+                    </div>
+                    <div class="result d-flex gap-5 align-items-center">
+                        <span>4</span>
+                        <span>-</span>
+                        <span>1</span>
+                    </div>
+                    <div class="team-two d-flex gap-5 align-items-center">
+                        <div class="team-name">
+                            <span>Tallakht</span>
+                        </div>
+                        <div class="team-logo">
+                            <img src="/assets/img/team-logo.png" alt="" width="50">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="match d-flex flex-column align-items-center">
+                <div class="time bg-secondary text-light text-center w-25">
+                    <span>Full time</span>
+                </div>
+                <div class="match-details match-full-time d-flex justify-content-around w-100 rounded-3">
+                    <div class="team-one d-flex gap-5 align-items-center">
+                        <div class="team-logo">
+                            <img src="/assets/img/team-logo2.png" alt="" width="50">
+                        </div>
+                        <div class="team-name">
+                            <span>Namek</span>
+                        </div>
+                    </div>
+                    <div class="result d-flex gap-5 align-items-center">
+                        <span>4</span>
+                        <span>-</span>
+                        <span>1</span>
+                    </div>
+                    <div class="team-two d-flex gap-5 align-items-center">
+                        <div class="team-name">
+                            <span>Tallakht</span>
+                        </div>
+                        <div class="team-logo">
+                            <img src="/assets/img/team-logo.png" alt="" width="50">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="secondary-content col-12 col-lg-3 ">
+    <div class="top-rating bg-light rounded pb-3">
+        <div class="p-3 mt-5 ">
+            <h3 class="text-center">Top rating</h3>
+            <div class="d-flex justify-content-between my-3">
+                <span>Khalid</span> <span>86%</span> <span>Namek</span>
+            </div>
+            <div class="d-flex justify-content-between my-3">
+                <span>Azeddine</span> <span>85%</span> <span>Aliens</span>
+            </div>
+            <div class="d-flex justify-content-between my-3">
+                <span>Khalil</span> <span>73%</span> <span>Error 404</span>
+            </div>
+        </div>
+        <div class="boutons d-flex justify-content-center">
+            <a class="btn btn-primary" href="#">View all players</a>
+        </div>
+    </div>
+
+
+    <div class="matchs bg-light rounded pb-3">
+        <div class="p-3 mt-5 ">
+            <h3 class="text-center">Matchs</h3>
+            <div class="d-flex justify-content-between my-3">
+                <span>Khalid</span> <span>25</span> <span>Namek</span>
+            </div>
+            <div class="d-flex justify-content-between my-3">
+                <span>Azeddine</span> <span>23</span> <span>Aliens</span>
+            </div>
+            <div class="d-flex justify-content-between my-3">
+                <span>Khalil</span> <span>19</span> <span>Error 404</span>
+            </div>
+        </div>
+        <div class="boutons d-flex justify-content-center">
+            <a class="btn btn-primary" href="#">View all Matchs</a>
+        </div>
+    </div>
+</div>
