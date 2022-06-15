39000067a00818a5a9c2fdb322f5e44e4f10bb81
TayebsouiniYcode <TayebsouiniYcode@gmail.com>
2022-06-15 20:42:24 +0100

    Teams
    Ajouter view addTeam pour l'ajout et l'edit avec un controller pour
    gerer les request de la création et la modification des équipes

--- controllers/TeamController.php
+++ controllers/TeamController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 use app\core\Application;
 use app\core\Controller;
 use app\core\Request;
+use app\models\TeamModel;
 
 class TeamController extends Controller
 {
@@ -14,6 +15,45 @@ class TeamController extends Controller
 
     public function create(Request $request)
     {
-        return $this->render('createTeam');
+        $team = new TeamModel();
+        if ($request->isGet())
+        {
+            return $this->render('addTeam', [
+                'model' => $team
+            ]);
+        }
+
+        if ($request->isPost())
+        {
+            $team->loadData($request->getBody());
+            if ($team->validate() && $team->save()) {
+                Application::$app->response->redirect('/teams');
+                return;
+            }
+        }
+    }
+
+    public function edit(Request $request)
+    {
+        $team = new TeamModel();
+
+        if ($request->isGet())
+        {
+            $team->select(4);
+            $team->loadData($team->dataList);
+            $team->dataList = null;
+            return $this->render('addTeam', [
+                'model' => $team
+            ]);
+        }
+
+        if ($request->isPost())
+        {
+            $team->loadData($request->getBody());
+            $team->update($team->id);
+            return $this->render('addTeam', [
+                'model' => $team
+            ]);
+        }
     }
 }
