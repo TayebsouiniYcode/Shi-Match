6f397588c6686a11b371eea87fa9a482c7235dcd
TayebsouiniYcode <TayebsouiniYcode@gmail.com>
2022-06-17 11:00:01 +0100

    ajouter le nom d'équipe de l'utilisateur sur le profile

--- views/profile.php
+++ views/profile.php
@@ -4,7 +4,7 @@
             <img class="player-image-cercle" src="/assets/img/player.jpg" alt="" width="200">
         </div>
         <div class="informations-header">
-            <h3 class="name">Tayeb SOUINI</h3>
+            <h3 class="name">Tayeb SOUINI (<?php echo $user->team->name; ?>)</h3>
         </div>
     </div>
     <div class="informations mt-5 row">
@@ -30,35 +30,35 @@
             <!-- <form action="" class="row"> -->
             <div class="row">
             <?php $form = app\core\form\Form::begin('', "post"); ?>
-                <input type="hidden" name="id" value="<?php echo $model->id;?>">
+                <input type="hidden" name="id" value="<?php echo $user->id;?>">
                 <fieldset class="mt-3 col-md-12">
                     <legend class="text-muted">Personel</legend>
                     <div class="form-group d-flex justify-content-between gap-2">
                         <div class="firstname w-50">
                             <!-- <label for="firstname" class="form-label">Firstname</label>
                             <input type="text" name="firstname" id="firstname" class="form-control" value="Tayeb"> -->
-                            <?php echo $form->field($model, 'firstname'); ?>
+                            <?php echo $form->field($user, 'firstname'); ?>
                         </div>
                         <div class="lastname w-50">
                             <!-- <label for="lastname" class="form-label">Lastname</label>
                             <input type="text" name="lastname" id="lastname" class="form-control" value="SOUINI"> -->
-                            <?php echo $form->field($model, 'lastname'); ?>
+                            <?php echo $form->field($user, 'lastname'); ?>
                         </div>
                     </div>
                     <div class="form-group">
                         <!-- <label for="date_of_birth" class="form-label">Date of birth</label>
                         <input type="date" name="date_of_birth" id="date_of_birth" class="form-control" value="Morocco"> -->
-                        <?php echo $form->field($model, 'age'); ?>
+                        <?php echo $form->field($user, 'age'); ?>
                     </div>
                     <div class="form-group">
                         <!-- <label for="country" class="form-label">Country</label>
                         <input type="text" name="country" id="country" class="form-control" value="Morocco"> -->
-                        <?php //echo $form->field($model, 'address'); ?>
+                        <?php //echo $form->field($user, 'address'); ?>
                     </div>
                     <div class="form-group">
                         <!-- <label for="city" class="form-label">City</label>
                         <input type="text" name="city" id="city" class="form-control" value="Youssoufia"> -->
-                        <?php //echo $form->field($model, 'city'); ?>
+                        <?php //echo $form->field($user, 'city'); ?>
                     </div>
                 </fieldset>
                 <fieldset class="mt-3 col-md-12">
@@ -66,12 +66,12 @@
                     <div class="form-group">
                         <!-- <label for="email" class="form-label">Email</label>
                         <input type="email" name="email" id="email" class="form-control" value="Tayebsouini.2@gmail.com"> -->
-                        <?php echo $form->field($model, 'email'); ?>
+                        <?php echo $form->field($user, 'email'); ?>
                     </div>
                     <div class="form-group">
                         <!-- <label for="phone" class="form-label">Phone number</label>
                         <input type="tel" name="phone" id="phone" class="form-control" value="0607189671"> -->
-                        <?php echo $form->field($model, 'phone'); ?>
+                        <?php echo $form->field($user, 'phone'); ?>
                     </div>
                 </fieldset>
                 <div class="buttons mt-5">
