a940844bdc205ee658e600088dfc36dc658147ed
TayebsouiniYcode <TayebsouiniYcode@gmail.com>
2022-06-25 18:33:35 +0100

    refactoring index divid routes

--- public/index.php
+++ public/index.php
@@ -8,8 +8,6 @@ use app\controllers\AdminController;
 use app\controllers\MatchController;
 use app\controllers\HelperController;
 use app\controllers\PlayerController;
-use app\controllers\SharedController;
-
 
 require_once __DIR__.'/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
@@ -25,103 +23,104 @@ $config = [
     
 $app = new Application(dirname(__DIR__), $config);
 
-$app->router->get('/welcomeYoucode', [HelperController::class, 'welcomeYoucode']);
-
-$app->router->get('/', [HelperController::class, 'index']);
-
-$app->router->get('/login', [AuthController::class, 'login']);
-$app->router->post('/login', [AuthController::class, 'login']);
-
-$app->router->get('/signup', [AuthController::class, 'signup']);
-$app->router->post('/signup', [AuthController::class, 'signup']);
-
-$app->router->get('/dashboard', [AdminController::class, 'dashboard']);
-
-$app->router->get('/teams', [TeamController::class, 'index']);
-$app->router->get('/teamDetails', [TeamController::class, 'details']);
-$app->router->get('/players', [PlayerController::class, 'players']);
-$app->router->get('/playerDetails', [PlayerController::class, 'playerDetails']);
-$app->router->get('/matchs', [PlayerController::class, 'matchs']);
-$app->router->get('/profile', [UserController::class, 'profile']);
-$app->router->post('/profile', [UserController::class, 'profile']);
-$app->router->get('/deleteTeam', [TeamController::class, 'destroy']);
-$app->router->get('/addPlayer', [PlayerController::class, 'addPlayer']);
-$app->router->post('/addPlayer', [PlayerController::class, 'addPlayer']);
-
-$app->router->get('/editPassword', [UserController::class, 'editPassword']);
-
-//Team routes
-$app->router->get('/addTeam', [TeamController::class, 'create']);
-$app->router->post('/addTeam', [TeamController::class, 'create']);
-
-$app->router->get('/editTeam', [TeamController::class, 'edit']);
-$app->router->post('/editTeam', [TeamController::class, 'edit']);
-$app->router->post('/updateTeamImage', [TeamController::class, 'updateImage']);
+// standards routes
+$app->router->get('/welcomeYoucode', 
+    [HelperController::class, 'welcomeYoucode'
+]);
+$app->router->get('/', [
+    HelperController::class, 'index'
+]);
+$app->router->get('/login', [
+    AuthController::class, 'login'
+]);
+$app->router->post('/login', [
+    AuthController::class, 'login'
+]);
+$app->router->get('/signup', [
+    AuthController::class, 'signup'
+]);
+$app->router->post('/signup', [
+    AuthController::class, 'signup'
+]);
 
 
-$app->router->get('/teamJoinRequest', [TeamController::class, 'joinRequest']);
+// admin routes
+$app->router->get('/dashboard', [
+    AdminController::class, 'dashboard'
+]);
 
 
-$app->router->get('/mtachRequest', [MatchController::class, 'create']);
-$app->router->post('/mtachRequest', [MatchController::class, 'create']);
+// team routes
+$app->router->get('/teams', [
+    TeamController::class, 'index'
+]);
+$app->router->get('/teamDetails', [
+    TeamController::class, 'details'
+]);
+$app->router->get('/deleteTeam', [
+    TeamController::class, 'destroy'
+]);
+$app->router->get('/addTeam', [
+    TeamController::class, 'create'
+]);
+$app->router->post('/addTeam', [
+    TeamController::class, 'create'
+]);
 
-$app->router->get('/logout', [AuthController::class, 'logout']);
+$app->router->get('/editTeam', [
+    TeamController::class, 'edit'
+]);
+$app->router->post('/editTeam', [
+    TeamController::class, 'edit'
+]);
+$app->router->post('/updateTeamImage', [
+    TeamController::class, 'updateImage'
+]);
+$app->router->get('/teamJoinRequest', [
+    TeamController::class, 'joinRequest'
+]);
+
+// player routes
+$app->router->get('/players', [
+    PlayerController::class, 'players'
+]);
+$app->router->get('/playerDetails', [
+    PlayerController::class, 'playerDetails'
+]);
+$app->router->get('/matchs', [
+    PlayerController::class, 'matchs'
+]);
+$app->router->get('/addPlayer', [
+    PlayerController::class, 'addPlayer'
+]);
+$app->router->post('/addPlayer', [
+    PlayerController::class, 'addPlayer'
+]);
 
 
+//user routes
+$app->router->get('/profile', [
+    UserController::class, 'profile'
+]);
+$app->router->post('/profile', [
+    UserController::class, 'profile'
+]);
+$app->router->get('/editPassword', [
+    UserController::class, 'editPassword'
+]);
 
-// $app->router->post('/dashboard', [AdminController::class, 'dashboard']);
-// $app->router->get('/login', [AuthController::class, 'login']);
-// $app->router->post('/login', [AuthController::class, 'login']);
-
-// $app->router->get('/register', [AuthController::class, 'register']);
-// $app->router->post('/register', [AuthController::class, 'register']);
-
-// $app->router->get('/updateUser', [UserController::class, 'updateUser']);
-// $app->router->post('/updateUser', [UserController::class, 'updateUser']);
-
-// $app->router->get('/userList', [UserController::class, 'userList']);
-// $app->router->get('/user', [UserController::class, 'getUser']);
-
-// $app->router->get('/deleteUser', [UserController::class, 'delete']);
-// // -------- Student 
-// $app->router->get('/student', [StudentController::class, 'studentList']);
-
-// $app->router->get('/addStudent', [StudentController::class, 'add']);
-// $app->router->post('/addStudent', [StudentController::class, 'add']);
-
-// $app->router->get('/updateStudent', [StudentController::class, 'update']);
-// $app->router->post('/updateStudent', [StudentController::class, 'update']);
-// $app->router->get('/deleteStudent', [StudentController::class, 'delete']);
+// match routes
+$app->router->get('/mtachRequest', [
+    MatchController::class, 'create'
+]);
+$app->router->post('/mtachRequest', [
+    MatchController::class, 'create'
+]);
 
 
-// // -------- Parent 
-// $app->router->get('/addParent', [ParentController::class, 'add']);
-// $app->router->post('/addParent', [ParentController::class, 'add']);
-
-
-// --------Teacher
-// $app->router->post('/updateTeacher', [TeacherController::class, 'updateTeacher']);
-// $app->router->get('/updateTeacher', [TeacherController::class, 'updateTeacher']);
-
-// $app->router->post('/addTeacher', [TeacherController::class, 'addTeacher']);
-// $app->router->get('/addTeacher', [TeacherController::class, 'addTeacher']);
-
-// $app->router->post('/teacher', [TeacherController::class, 'teacherList']);
-// $app->router->get('/teacher', [TeacherController::class, 'teacherList']);
-
-// $app->router->get('/deleteTeacher', [TeacherController::class, 'deleteTeacher']);
-
-// ------end Teacher
-
-// ------ class
-// $app->router->get('/class', [ClassController::class, 'classList']);
-
-// $app->router->get('/addClass', [ClassController::class, 'add']);
-// $app->router->post('/addClass', [ClassController::class, 'add']);
-
-// $app->router->get('/updateClass', [ClassController::class, 'update']);
-// $app->router->post('/updateClass', [ClassController::class, 'update']);
-// $app->router->get('/deleteClass', [ClassController::class, 'delete']);
-
+// Authentication routes
+$app->router->get('/logout', [
+    AuthController::class, 'logout'
+]);
 
 $app->run();
